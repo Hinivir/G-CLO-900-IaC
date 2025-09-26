c3f97b1de3bfc39ff88618f62e3ed6b695493f40
Skaahn <stanislas.commenge@epitech.eu>
2025-09-26 12:17:22 +0200

    feat: add iam terraform project

---
+++ iam/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  type        = string
+  description = "Cloud project ID"
+}
+
+variable "region" {
+  type        = string
+  description = "Region for resources"
+}
+
+variable "zone" {
+  type        = string
+  description = "Zone for resources"
+}
