8b95011aa7bf288879c785a7e3cc50be4a721c0c
Raymondbty <amaury.bariety@gmail.com>
2025-10-03 15:56:41 +0200

    feat: update infra side with Go practice

--- infra/VM.tf
+++ infra/VM.tf
@@ -1,16 +1,42 @@
-resource "google_compute_instance" "default" {
-  name         = "my-vm"
-  machine_type = "n1-standard-1"
-  zone         = "us-central1-a"
+resource "google_compute_instance" "api_vm" {
+  name         = "api-vm"
+  machine_type = "e2-micro"
+  zone         = var.zone
+  project      = var.project_id
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-minimal-2210-kinetic-amd64-v20230126"
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
     }
   }
 
   network_interface {
-    network = "default"
-    access_config {}
+    network       = "default"
+    access_config {} # donne une IP publique
   }
+
+  tags = ["api-server"]
+
+  metadata_startup_script = <<-EOT
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y docker.io
+
+    # Télécharge l'image Docker de l'API Go (buildée et pushée avant)
+    docker run -d -p 80:18080 gcr.io/${var.project_id}/api:latest
+  EOT
+}
+
+# Firewall pour autoriser l'accès HTTP
+resource "google_compute_firewall" "api_firewall" {
+  name    = "allow-api-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  target_tags   = ["api-server"]
+  source_ranges = ["0.0.0.0/0"]
 }
--- infra/api.tf
+++ infra/api.tf
@@ -3,3 +3,26 @@ resource "google_api_gateway_api" "api" {
   api_id   = var.api_id
   project  = var.project_id
 }
+
+resource "google_api_gateway_api_config" "api_config" {
+  provider     = google-beta
+  api          = google_api_gateway_api.api.api_id
+  api_config_id = "v1"
+  project      = var.project_id
+
+  openapi_documents {
+    document {
+      path     = "openapi.yaml"
+      contents = file("${path.module}/openapi.yaml")
+    }
+  }
+}
+
+resource "google_api_gateway_gateway" "api_gateway" {
+  provider   = google-beta
+  project    = var.project_id
+  api        = google_api_gateway_api.api.api_id
+  api_config = google_api_gateway_api_config.api_config.id
+  gateway_id = "api-gateway"
+  region     = var.region
+}
