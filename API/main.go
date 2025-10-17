00357a0ab4a312adf0ed5e581425526c9db07f12
Raymondbty <amaury.bariety@gmail.com>
2025-10-17 11:03:48 +0200

    feat:connect API.go to DB

--- API/main.go
+++ API/main.go
@@ -1,93 +1,95 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "math/rand"
-    "net/http"
-    "strconv"
-    "github.com/gorilla/mux"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 type Data struct {
-    ID      string `json:"id"`
-    Title   string `json:"title"`
-    Content string `json:"content"`
+	ID      string `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
 	DueDate string `json:"due_date"`
-	Done   bool   `json:"done"`
+	Done    bool   `json:"done"`
 }
 
-var datas []Data
+func getDatas(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	rows, err := db.Query("SELECT id, title, content, due_date, done FROM datas ORDER BY id")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
 
-func getDatas(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(datas)
+	var datas []Data
+	for rows.Next() {
+		var d Data
+		if err := rows.Scan(&d.ID, &d.Title, &d.Content, &d.DueDate, &d.Done); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		datas = append(datas, d)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(datas)
 }
 
-func getData(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    params := mux.Vars(r)
-    for _, item := range datas {
-        if item.ID == params["id"] {
-            json.NewEncoder(w).Encode(item)
-            return
-        }
-    }
-    http.NotFound(w, r)
-}
+func createData(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	var d Data
+	_ = json.NewDecoder(r.Body).Decode(&d)
+	d.ID = strconv.Itoa(rand.Intn(100000))
 
-func createData(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    var data Data
-    _ = json.NewDecoder(r.Body).Decode(&data)
-    data.ID = strconv.Itoa(rand.Intn(100000))
-    datas = append(datas, data)
-    json.NewEncoder(w).Encode(data)
-}
+	_, err := db.Exec(
+		"INSERT INTO datas (id, title, content, due_date, done) VALUES ($1, $2, $3, $4, $5)",
+		d.ID, d.Title, d.Content, d.DueDate, d.Done,
+	)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-func updateData(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    params := mux.Vars(r)
-    for index, item := range datas {
-        if item.ID == params["id"] {
-            datas = append(datas[:index], datas[index+1:]...)
-            var data Data
-            _ = json.NewDecoder(r.Body).Decode(&data)
-            data.ID = params["id"]
-            datas = append(datas, data)
-            json.NewEncoder(w).Encode(data)
-            return
-        }
-    }
-    http.NotFound(w, r)
-}
-
-func deleteData(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    params := mux.Vars(r)
-    for index, item := range datas {
-        if item.ID == params["id"] {
-            datas = append(datas[:index], datas[index+1:]...)
-            json.NewEncoder(w).Encode(map[string]string{"message": "Données supprimées"})
-            return
-        }
-    }
-    http.NotFound(w, r)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(d)
 }
 
 func main() {
-    datas = append(datas, Data{ID: "1", Title: "Première data", Content: "Contenu de data 1"})
-    datas = append(datas, Data{ID: "2", Title: "Deuxième data", Content: "Contenu de data 2"})
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
 
-    r := mux.NewRouter()
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName,
+	)
 
-    r.HandleFunc("/data", getDatas).Methods("GET")
-    r.HandleFunc("/data/{id}", getData).Methods("GET")
-    r.HandleFunc("/data", createData).Methods("POST")
-    r.HandleFunc("/data/{id}", updateData).Methods("PUT")
-    r.HandleFunc("/data/{id}", deleteData).Methods("DELETE")
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Erreur de connexion à la base :", err)
+	}
+	defer db.Close()
 
-    fmt.Println("Serveur démarré sur le port 8000")
-    log.Fatal(http.ListenAndServe(":8000", r))
+	if err := db.Ping(); err != nil {
+		log.Fatal("Impossible de se connecter à la base :", err)
+	}
+
+	fmt.Println("Connexion PostgreSQL réussie")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) { getDatas(w, r, db) }).Methods("GET")
+	r.HandleFunc("/data", func(w http.ResponseWriter, r *http.Request) { createData(w, r, db) }).Methods("POST")
+
+	fmt.Println("Serveur démarré sur le port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
